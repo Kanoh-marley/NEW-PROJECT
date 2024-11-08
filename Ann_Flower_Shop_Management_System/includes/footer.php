5a6c367596630376089b9295bd5133b3b8754e51
Kanoh-marley <njugunaemmanuel16@gmail.com>
2024-11-08 12:04:26 +0300

    Add files via upload

---
+++ Ann_Flower_Shop_Management_System/includes/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.webp" alt="">
+         <h3>our email</h3>
+         <!-- <a href="mailto:ann@gmail.com">ann@gmail.com</a> -->
+         <a href="mailto:annfloweri@gmail.com">annflower@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.webp" alt="">
+         <h3>opening hours</h3>
+         <p>8:00am to 5:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.webp" alt="">
+         <h3>our address</h3>
+         <a href="#">Kawangware</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.webp" alt="">
+         <h3>our number</h3>
+         <!-- <a href="tel:1234567890">0710986531</a> -->
+         <a href="tel:0791336118">0791336118</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; <?= date('Y'); ?> by <span>Manu</span></div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/flower-loader.gif" alt="">
+</div>
